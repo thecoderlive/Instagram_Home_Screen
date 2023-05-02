11e8a5d28a53171584e56864ac5025ccc83508a5
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-02 19:31:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,73 @@
+export const item = {
+    "id": "cfef943b-8bda-495e-b891-e53ef1718382",
+    "app_name": "believe",
+    "story_list": [
+        {
+            "id": "a12f1000-fa66-4f71-9756-d7d416353262",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png",
+            "user_name": "officer"
+        },
+        {
+            "id": "e785846a-fdc6-49f3-9582-03bb99ee4b37",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png",
+            "user_name": "either"
+        },
+        {
+            "id": "91d7d92f-4351-49df-8ad2-a71e805d7728",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png",
+            "user_name": "color"
+        },
+        {
+            "id": "5cf7e564-a4c2-43da-bbad-741a283dd932",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png",
+            "user_name": "executive"
+        },
+        {
+            "id": "0cfaf5b4-319e-432c-8ee6-cd4c17f20667",
+            "story_image_list": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png",
+            "user_name": "likely"
+        }
+    ],
+    "feed_list": [
+        {
+            "id": "291ce5be-8afb-4a49-bd01-27215dfafa00",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png",
+            "num_like": "carry",
+            "description": "Line state clear trouble. Leave enter store whether TV real safe. Score month trouble today bill."
+        },
+        {
+            "id": "ddc956ba-c960-4559-b78c-81a7389bef0f",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png",
+            "num_like": "evidence",
+            "description": "Exist ahead political again agency. Letter recently program find check. Total certain picture though feeling."
+        },
+        {
+            "id": "83b7782f-a41d-4b92-b7e9-ae0883e139f4",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png",
+            "num_like": "these",
+            "description": "Realize fire capital there believe family possible. That business short campaign month. Professor everything early cause."
+        },
+        {
+            "id": "209f3ed3-9cf5-4e38-96ae-9000cca9e6ae",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png",
+            "num_like": "so",
+            "description": "Loss some need tough enter baby than. Explain doctor thing degree wind nice design."
+        },
+        {
+            "id": "fffefb7e-58ac-4398-8131-c6a58f7da116",
+            "post_user": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png",
+            "news_feed": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png",
+            "like_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png",
+            "num_like": "cost",
+            "description": "Father themselves newspaper community continue. Stuff specific media several to produce another. Establish work everything together spring store."
+        }
+    ]
+}
